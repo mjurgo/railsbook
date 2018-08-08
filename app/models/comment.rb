043f2a98b345b1a8f1d084e3b6f37d4356c37ba9
vonMajkel <mpjurgo@gmail.com>
2018-08-08 13:00:20 +0200

    Implement associations for commenting posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 end
