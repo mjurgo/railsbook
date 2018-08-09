45f038f31e25c3778f73693a6a788a29cffe95cb
vonMajkel <mpjurgo@gmail.com>
2018-08-09 14:31:20 +0200

    Create users controller with index and show actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
   root to: 'static_pages#index'
 
   resources :likes
   resources :friendships, only: [:create, :update, :destroy]
   devise_for :users
+  resources :users, only: [:index, :show]
 
   match 'like',to: 'likes#like', via: :post
   match 'dislike', to: 'likes#dislike', via: :delete
