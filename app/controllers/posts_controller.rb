5d7c2c00494eaa35425da52c5a08e23421634365
vonMajkel <mpjurgo@gmail.com>
2018-08-13 14:00:21 +0200

    Add new post form and posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,30 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Post added!"
+    else
+      redirect_to posts_path, notice: "Sorry! Post couldn't be created!"
+    end
+  end
+
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#index'
 
-  resources :posts, only: [:show]
+  resources :posts, only: [:show, :index, :new, :create]
   resources :likes
   resources :friendships, only: [:create, :update, :destroy]
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
